f91ac14d2c4f63523c4eda7a9f5e699c13d92830
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-22 17:32:11 +0900

    refactor: 서로 다른 세자리 숫자 만드는 기능 오류 시 다시 입력 받도록 리팩토링

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,11 +1,26 @@
 package baseball.controller;
 
+import baseball.domain.GameNumber;
 import baseball.view.InputView;
 
 public class Game {
 
     public void play() {
-        System.out.println(InputView.readNumber());
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
+        System.out.print("숫자를 입력해주세요 : ");
+        GameNumber userNumber = createGameNumber();
+
+
+    }
+
+    private GameNumber createGameNumber() {
+        while (true) {
+            try {
+                return new GameNumber(InputView.read());
+            } catch (IllegalArgumentException error) {
+                System.out.println("다시 입력해 주세요.");
+            }
+        }
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,15 +3,7 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static int readNumber() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException error) {
-            System.out.println("[ERROR] 숫자로만 입력해 주세요.");
-            return readNumber();
-        }
+    public static String read() {
+        return Console.readLine();
     }
 }
