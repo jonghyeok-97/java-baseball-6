1750589acba4f58d2959e3046e6d6d5271a6b432
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-25 17:19:11 +0900

    feat: 랜덤으로 생성되는 숫자 받는 기능 구현

---
+++ src/main/java/baseball/domain/GameNumber.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+public class GameNumber {
+    //private User user;
+    private Computer computer;
+
+    public void create() {
+        int randomNumber = computer.select();
+    }
+
+}
